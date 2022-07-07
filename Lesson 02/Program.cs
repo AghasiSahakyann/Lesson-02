6e3d4b4fdd611a3a6b76a36379c53ccbc2608a36
Sahakyan <aghassahakyan66@gmail.com>
2022-07-08 01:23:42 +0400

    Second exercise

--- Lesson 02/Program.cs
+++ Lesson 02/Program.cs
@@ -7,11 +7,17 @@ namespace Lesson_02
         static void Main(string[] args)
         {
             // 1.
-            Console.WriteLine("Write text");
-            string text = Console.ReadLine();
-            Console.WriteLine("Your text was following - " + text);
+            //Console.WriteLine("Write text");
+            //string text = Console.ReadLine();
+            //Console.WriteLine("Your text was following - " + text);
 
             // 2.
+            Console.WriteLine("Write text");
+            string text = Console.ReadLine();
+            int length = text.Length;
+            Console.WriteLine("Your text was length - " + length);
+
+
         }
     }
 }
