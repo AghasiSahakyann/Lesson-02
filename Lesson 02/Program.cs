c89017c208750dd834a5a3967152ff0d34b7e169
Sahakyan <aghassahakyan66@gmail.com>
2022-07-08 01:15:15 +0400

    First exercise

---
+++ Lesson 02/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lesson_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1.
+            Console.WriteLine("Write text");
+            string text = Console.ReadLine();
+            Console.WriteLine("Your text was following - " + text);
+
+            // 2.
+        }
+    }
+}
